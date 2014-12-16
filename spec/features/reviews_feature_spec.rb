66f7f2baf759413f8ffd4f8227922b476316fe84
ctrembath <claretrembath@ymail.com>
2014-12-16 16:58:21 +0000

    can add review

---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'reviewing' do
+  before { Restaurant.create(name: 'KFC') }
+
+  it 'allows users to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: 'so so'
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('so so')
+  end
+
+end
