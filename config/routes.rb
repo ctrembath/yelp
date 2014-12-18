9330ad11947ed6506560722ad67c6b9f499849c6
ctrembath <claretrembath@ymail.com>
2014-12-18 11:17:25 +0000

    fixed nested routes reviews in restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   
 
 
-  # resources :users do
-  #   resources :restaurants
-  # end
+  resources :restaurants do
+    resources :reviews
+  end
 
   root to: "restaurants#index"
 
